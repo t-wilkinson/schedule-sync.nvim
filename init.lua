1870b1d2b6ff109b72bf56d9fd6c0654716f522a
t-wilkinson <winston.trey.wilkinson@gmail.com>
2025-05-29 09:51:08 -0400

    Add vim command

--- init.lua
+++ init.lua
@@ -1,20 +1,20 @@
-local job = require("plenary.job")
+-- local job = require("plenary.job")
 
-vim.api.nvim_create_user_command("SyncSchedule", function()
-	local current_buffer_filename = vim.fn.expand("%:p")
-	local venv_activate_cmd = "source " .. vim.fn.getcwd() .. "/.venv/bin/activate"
-	job:new({
-		command = "bash",
-		args = {
-			"-c",
-			venv_activate_cmd .. " && python3 -m calendar_sync.sync_schedule " .. current_buffer_filename,
-		},
-		on_exit = function(j, return_code)
-			if return_code == 0 then
-				print("Schedule synced successfully!")
-			else
-				print("Error syncing schedule. Check your_script.py for details.")
-			end
-		end,
-	}):start()
-end, {})
+-- vim.api.nvim_create_user_command("SyncSchedule", function()
+-- 	local current_buffer_filename = vim.fn.expand("%:p")
+-- 	local venv_activate_cmd = "source " .. vim.fn.getcwd() .. "/.venv/bin/activate"
+-- 	job:new({
+-- 		command = "bash",
+-- 		args = {
+-- 			"-c",
+-- 			venv_activate_cmd .. " && python3 -m calendar_sync.sync_schedule " .. current_buffer_filename,
+-- 		},
+-- 		on_exit = function(j, return_code)
+-- 			if return_code == 0 then
+-- 				print("Schedule synced successfully!")
+-- 			else
+-- 				print("Error syncing schedule. Check your_script.py for details.")
+-- 			end
+-- 		end,
+-- 	}):start()
+-- end, {})
